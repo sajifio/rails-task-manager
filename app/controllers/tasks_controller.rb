3657dbf63b64bdfe2901fbc584c45443ff2fa638
Fiorella Salazar <sajifio@gmail.com>
2022-02-15 17:56:16 +0000

    Add methods

--- app/controllers/tasks_controller.rb
+++ app/controllers/tasks_controller.rb
@@ -1,10 +1,12 @@
 class TasksController < ApplicationController
+  before_action :set_task, only: [:show, :edit, :update, :delete]
+
+
   def index
     @tasks = Task.all
   end
 
   def show
-    @task = Task.find(params[:id])
   end
 
   def new
@@ -13,7 +15,21 @@ class TasksController < ApplicationController
 
   def create
     @task = Task.create(strong_params)
+    @task.save
     redirect_to task_path(@task)
+  end
+
+  def edit
+  end
+
+  def update
+    @task.update(strong_params)
+    redirect_to task_path(@task)
+  end
+
+  def destroy
+    @task.destroy
+    redirect_to tasks_path
   end
 
   private
